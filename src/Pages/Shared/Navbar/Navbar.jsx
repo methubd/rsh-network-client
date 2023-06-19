e4bd4d14e440878e6f8d02dff5d7b5a368bf03f2
T R Methu <fmethubd@gmail.com>
2023-06-19 10:10:17 +0600

    responsive navbar added

---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+import './Navbar.css'
+import { useState } from 'react';
+
+const Navbar = () => {
+    const [menu, setMenu] = useState(false);
+    const menuItems = <>
+                    <Link className='menu-item' to='/'>Home</Link>
+                    <Link className='menu-item' >Appointments</Link>
+                    <Link className='menu-item' >Services</Link>
+                    <Link className='menu-item' >Contact</Link>
+                    </>
+
+    const handleExpandMenu = () => {
+        console.log('btn clicked');
+        console.log(menu);
+        return menuItems;
+    }
+    return (
+        <nav>
+            <div className='logo-container'>
+                <img className='logo' src="https://i.ibb.co/k8qL7Jf/logo.png" alt="" />
+            </div>
+            <div className='menu-container'>
+                {menuItems}
+                
+            </div>
+                {/* Menu Toggle Btn */}
+                { menu ?
+                <XMarkIcon onClick={() => handleExpandMenu(setMenu(!menu))} className='menu-icon'/>
+                :
+                <Bars3Icon onClick={() => handleExpandMenu(setMenu(!menu))} className='menu-icon'/> 
+                }
+                
+
+            { menu ? 
+            <div className='expanded-menu'>
+                    {menuItems}
+            </div> : " "
+            }
+        </nav>
+    );
+};
+
+export default Navbar;
