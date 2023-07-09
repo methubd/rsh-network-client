876b24e9ef362fa61bb105bcac9d2349b661ef81
T R Methu <fmethubd@gmail.com>
2023-07-09 20:40:05 +0600

    admin verification created, functionanily working well need to secure the APIs

--- src/Pages/Dashboard/Aside/Aside.jsx
+++ src/Pages/Dashboard/Aside/Aside.jsx
@@ -2,14 +2,22 @@ import { ArrowLeftIcon } from '@heroicons/react/24/solid'
 
 import { Link } from 'react-router-dom';
 import './Aside.css'
+import useAdmin from '../../../Hooks/useAdmin';
+import useAuth from '../../../Hooks/useAuth';
 
 const Aside = () => {
+    const {user} = useAuth();
+    const [isAdmin] = useAdmin();
+
     return (
         <aside>
             
             <button>Hide</button>
             
             {/* User Routes */}
+            { user &&
+                
+            <>
             <Link className='dashboard-btn btn-primary' to='/dashboard/'>Home</Link>
             <Link className='dashboard-btn btn-primary' to='/dashboard/'>Prescription</Link>
             <Link className='dashboard-btn btn-primary' to='/dashboard/pending-appointment'>Pending Appointment</Link>
@@ -18,12 +26,19 @@ const Aside = () => {
             <Link className='dashboard-btn btn-primary' to='/dashboard/'>Payment History</Link>
             <Link className='dashboard-btn btn-primary' to='/dashboard/'>Appointment History</Link>
             <hr className='hr-line' />
+            </>
+            
+            }
             
             {/* Admin Routes */}
-            <Link className='dashboard-btn btn-primary' to='/dashboard/manage-users'>Manage Users</Link>
-            <Link className='dashboard-btn btn-primary' to='/dashboard/manage-consultant'>Manage Consultants</Link>
+            {  isAdmin &&
+            <>
+                <Link className='dashboard-btn btn-primary' to='/dashboard/manage-users'>Manage Users</Link>
+                <Link className='dashboard-btn btn-primary' to='/dashboard/manage-consultant'>Manage Consultants</Link>
+                <hr className='hr-line' />
+            </>
+            }
 
-            <hr className='hr-line' />
             <Link className='dashboard-btn btn-secondary btn-with-icon' to='/'>Go Back <ArrowLeftIcon className='back-icon'/> </Link>
         </aside>
     );
